08abf7c08cca5ba4c5bf630465da339e288deb61
Renato <42098236+renato-sanseverino@users.noreply.github.com>
2022-08-21 12:34:47 -0300

    Ajustes

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ export default function App() {
   return (
     <div style={{ "background-image": "url('images/sala_de_aula.jpg')", "background-size": "100%"}}>
         <header className="header">      
-            <img src="images\logo.png" style={{"width":"400px", "height":"400px", "padding": "50px"}} />
+            <img src="images\logo.png" style={{"width":"250px", "height":"250px", "padding": "30px"}} />
         </header>
         <main className="main">
             <TeacherList></TeacherList>
