fa060e37b63cb6629535fb6f8d1004b901b8260b
Renato <42098236+renato-sanseverino@users.noreply.github.com>
2022-10-29 17:25:04 -0300

    Ajustes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
 import Home from './pages/Home'
+import Students from './pages/Students'
 import Teachers from './pages/Teachers'
 import React, { useEffect } from 'react'
 import { Routes, Route, useLocation, Navigate } from 'react-router-dom'
+
+
+// React Router doesn´t seem to work on vercel
+// Possible fix: add  vercel config file  (vercel.json)
 
 
 export default function App() {
@@ -18,6 +23,7 @@ export default function App() {
       <Routes>
         <Route path="*" element={<Navigate to="/" replace />} />
         <Route exact path="/" element={<Home />} />
+        <Route path="/students" element={<Students />} />
         <Route path="/teachers" element={<Teachers />} />
       </Routes>
     </>
